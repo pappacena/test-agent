5b2f68fcb7bdc20c132fb081b6759cddd35fed03
Thiago F Pappacena <pappacena@gmail.com>
2024-04-11 21:11:41 -0300

    Tests

--- test_agent/main.py
+++ test_agent/main.py
@@ -6,6 +6,14 @@ from transformers.models.llama.tokenization_llama_fast import LlamaTokenizerFast
 from langchain.llms.base import LLM
 from langchain.callbacks.manager import CallbackManagerForLLMRun
 from typing import Optional, List, Mapping, Any
+import numexpr as ne
+from langchain.tools import WikipediaQueryRun, BaseTool
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.agents import Tool
+from langchain.agents import create_json_chat_agent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
 
 model_name = "mistralai/Mistral-7B-Instruct-v0.2"
 
@@ -67,3 +75,96 @@ class CustomLLMMistral(LLM):
 
 
 llm = CustomLLMMistral(model=model, tokenizer=tokenizer)
+
+wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=2000))
+
+class Calculator(BaseTool):
+    name = "calculator"
+    description = "Use this tool for math operations. It requires numexpr syntax. Use it always you need to solve any math operation. Be sure syntax is correct."
+
+    def _run(self, expression: str):
+      try:
+        return ne.evaluate(expression).item()
+      except Exception:
+        return "This is not a numexpr valid syntax. Try a different syntax."
+
+    def _arun(self, radius: int):
+        raise NotImplementedError("This tool does not support async")
+
+tools = [
+    Tool(
+        name="wikipedia",
+        description="Never search for more than one concept at a single step. If you need to compare two concepts, search for each one individually. Syntax: string with a simple concept",
+        func=wikipedia.run
+    ), 
+    Calculator(),
+]
+
+
+system="""
+You are designed to solve tasks. Each task requires multiple steps that are represented by a markdown code snippet of a json blob.
+The json structure should contain the following keys:
+thought -> your thoughts
+action -> name of a tool
+action_input -> parameters to send to the tool
+
+These are the tools you can use: {tool_names}.
+
+These are the tools descriptions:
+
+{tools}
+
+If you have enough information to answer the query use the tool "Final Answer". Its parameters is the solution.
+If there is not enough information, keep trying.
+
+"""
+
+human="""
+Add the word "STOP" after each markdown snippet. Example:
+
+```json
+{{"thought": "<your thoughts>",
+ "action": "<tool name or Final Answer to give a final answer>",
+ "action_input": "<tool parameters or the final output"}}
+```
+STOP
+
+This is my query="{input}". Write only the next step needed to solve it.
+Your answer should be based in the previous tools executions, even if you think you know the answer.
+
+These were the previous steps given to solve this query and the information you already gathered:
+"""
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        MessagesPlaceholder("chat_history", optional=True),
+        ("human", human),
+        MessagesPlaceholder("agent_scratchpad")
+    ]
+)
+
+agent = create_json_chat_agent(
+    tools = tools,
+    llm = llm,
+    prompt = prompt,
+    stop_sequence = ["STOP"],
+    template_tool_response = "{observation}"
+)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+
+# questions = [ 
+#     "What is 23 plus 17?",
+#     "What is the capital of France?",
+#     "Who was the inventor of the Radio?",
+#     "What is the double of the population of Madrid?",
+#     "Who is older, Tom Hanks or Kevin Costner?",
+# ]
+# for question in questions:
+#     print(agent_executor.invoke({"input": question}))
+
+while True:
+    question = input("Ask me something >> ")
+    answer = agent_executor.invoke({"input": question})
+    import ipdb; ipdb.set_trace()
